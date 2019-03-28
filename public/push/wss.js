efa6df02c214c4b66d608a3b87dae3c7af1328ec
Alexis Lepoutre <alepoutre@itroom.fr>
2019-03-28 14:58:23 +0100

    ajout de node

--- public/push/wss.js
+++ public/push/wss.js
@@ -89,6 +89,7 @@ wsServer.on( 'request', function(request) {
     } )
 
     connection.on('close', function(reasonCode, description) {
+        console.log(description);
         console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
     } )
 } )
